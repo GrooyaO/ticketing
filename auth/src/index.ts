d90c5e8f718937042153cc10c77e17241171980a
Stevan Grujic <s.grujic@rs.emakina.com>
2021-03-30 09:39:34 +0200

    added ingress nginx controller

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,10 @@ import { json } from 'body-parser';
 
 const app = express();
 app.use(json);
+app.get('api/users/currentuser', (req, res) => {
+  res.send('hi thereee');
+});
 
 app.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log('listening on port 3000s');
 });

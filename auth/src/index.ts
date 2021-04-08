87a068e00e880686b4222ee327e732ff523344e8
Stevan Grujic <s.grujic@rs.emakina.com>
2021-04-08 23:28:57 +0200

    changes to ingress-srv host name

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import { json } from 'body-parser';
 
 const app = express();
-app.use(json);
-app.get('api/users/currentuser', (req, res) => {
+app.use(json());
+app.get('/api/users/currentuser', (req, res) => {
   res.send('hi thereee');
 });
 
 app.listen(3000, () => {
-  console.log('listening on port 3000s');
+  console.log('listening on port 3000');
 });

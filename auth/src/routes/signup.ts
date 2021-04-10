872460f11e34922ed0a9635bc7e604f2d2ba6c1d
Stevan Grujic <s.grujic@rs.emakina.com>
2021-04-10 21:00:24 +0200

    error handler init

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -16,12 +16,12 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new Error('Invalid email or password');
     }
 
-    const { email, password } = req.body;
+    console.log('Creating a user ');
+    throw new Error('Error connecting to db');
 
-    console.log('Creating a user...');
     res.send({});
   }
 );

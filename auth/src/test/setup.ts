ec4ec1e0985bf9a94bb60aca7ce96215cc6a83fe
Stevan Grujic <s.grujic@rs.emakina.com>
2021-04-19 19:03:44 +0200

    first test for singup

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,10 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 import { app } from '../app';
 
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = 'asdsa';
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 

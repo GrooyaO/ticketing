a6b0e8a13427ae36c3e3dadead91f6c368878268
Stevan Grujic <s.grujic@rs.emakina.com>
2021-04-19 22:14:04 +0200

    more singout tests,current-user testing,signin tests

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -17,7 +17,27 @@ it('fails when an password that does not exist is supplied', async () => {
     .post('/api/users/signin')
     .send({
       email: 'test@test.com',
-      password: 'aslkdfjalskdfj',
+      password: 'aasdsawq',
     })
     .expect(400);
+});
+
+it('responds with a cookie when given valid credentials', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(200);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
 });

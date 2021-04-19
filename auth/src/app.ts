ea4240afa609139941de9f3b3adc443267422cb6
Stevan Grujic <s.grujic@rs.emakina.com>
2021-04-19 18:50:50 +0200

    setup tests for auth

---
+++ auth/src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+//trust https proxy such as ngnix
+app.set('trust proxy', true);
+app.use(json());
+//disable encryption on cookie since JWT is already enrypted
+//cookie is only used over https connection
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };

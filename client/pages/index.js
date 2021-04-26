c21f06f87c24e28fd6a76813e497b26d6eb2d38f
Stevan Grujic <s.grujic@rs.emakina.com>
2021-04-26 23:38:57 +0200

    exposed host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,32 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
+  //console.log(currentUser);
   return <h1> Landing Page</h1>;
 };
 
 LandingPage.getInitialProps = async () => {
-  const response = await axios.get('https://ticketing.dev/api/users/currentuser');
+  if (typeof window === 'undefined') {
+    //srv
+    //make req to ingress.ngnix...
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+    return data;
+  } else {
+    //broswer
+    //make req to base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
 
-  return response.data;
+    return data;
+  }
+
+  return {};
 };
 
 export default LandingPage;

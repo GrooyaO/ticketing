bf47e21be6a196371d1cd96498f7afc43b35aa43
Stevan Grujic <s.grujic@rs.emakina.com>
2021-05-03 23:11:08 +0200

    common error handling logic extracted into library

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@grooyatickets/common';
 
 const router = express.Router();
 

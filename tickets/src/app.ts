b951e64aa04ba375a83f236b5c4fa8fddb2a1f59
Stevan Grujic <s.grujic@rs.emakina.com>
2021-05-04 23:06:19 +0200

    show tickets:id test and logic, common lib update related to error logging

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@grooyatickets/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 //trust https proxy such as ngnix
@@ -20,6 +21,7 @@ app.use(
 
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();

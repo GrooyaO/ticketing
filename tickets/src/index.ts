efee67c13c3796e88217eb541c133fa7a2fa45ae
Stevan Grujic <s.grujic@rs.emakina.com>
2021-05-04 20:57:18 +0200

    extracted mongo_uri into env var for auth and tickets serv

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,13 +5,16 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('mongo uri not defined');
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log('Connected to mongodb');
+    console.log('Connected to mongodb tickets');
   } catch (err) {
     console.error(err);
   }

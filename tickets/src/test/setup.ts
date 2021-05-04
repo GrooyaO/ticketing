655a9220905d91d528d9f01d7c758a184c5bebfd
Stevan Grujic <s.grujic@rs.emakina.com>
2021-05-05 00:06:02 +0200

    edit ticket test and logic

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -42,7 +42,7 @@ afterAll(async () => {
 global.signin = () => {
   //Build a JWT payload {id,email}
   const payload = {
-    id: '123jkl',
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
   //Create JWT

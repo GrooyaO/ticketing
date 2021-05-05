c18c97a3c3668983c3eee18e425583332848cd89
Stevan Grujic <s.grujic@rs.emakina.com>
2021-05-05 23:11:00 +0200

    killing process if its closed imemediately

--- nats-ss-playground/src/listener.ts
+++ nats-ss-playground/src/listener.ts
@@ -8,6 +8,11 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+
   const options = stan.subscriptionOptions().setManualAckMode(true);
 
   const subscription = stan.subscribe(
@@ -26,3 +31,6 @@ stan.on('connect', () => {
     msg.ack();
   });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());

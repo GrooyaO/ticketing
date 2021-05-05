6247b9d2bd7b984f0ade0b53c2697baf06d16993
Stevan Grujic <s.grujic@rs.emakina.com>
2021-05-05 21:18:08 +0200

    setup listener

--- nats-ss-playground/src/publisher.ts
+++ nats-ss-playground/src/publisher.ts
@@ -1,4 +1,4 @@
-import nats, { Stan } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
@@ -6,4 +6,14 @@ const stan = nats.connect('ticketing', 'abc', {
 
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
+
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20,
+  });
+
+  stan.publish('ticket:created', data, () => {
+    console.log('event published');
+  });
 });

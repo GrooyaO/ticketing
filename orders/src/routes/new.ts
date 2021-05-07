289853a9cb85a3c1dcb2aaca7283b651c74785ee
Stevan Grujic <s.grujic@rs.emakina.com>
2021-05-07 14:23:19 +0200

    order model, ticket model for order serivce,new order router

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { validateRequest, requireAuth } from '@grooyatickets/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('Ticket id must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };

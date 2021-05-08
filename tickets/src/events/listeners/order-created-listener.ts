55b5900ec9890c684667f0b1cf0b9876bdfd2982
Stevan Grujic <s.grujic@rs.emakina.com>
2021-05-08 16:51:08 +0200

    order created listener

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,10 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@grooyatickets/common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+}

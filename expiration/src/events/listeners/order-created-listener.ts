7f052d983486c217944c63c6dd2cba427cbc0ef2
Stevan Grujic <s.grujic@rs.emakina.com>
2021-05-10 19:31:18 +0200

    exp-complete listener done + test

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -9,7 +9,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
     const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
-    console.log('waiting this many milliseconds to process the jobb:', delay);
+    console.log('waiting this many milliseconds to process the job:', delay);
     await expirationQueue.add(
       {
         orderId: data.id,

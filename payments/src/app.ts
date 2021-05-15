a446316db4fadac32d4ed83dc6cc1af96af5a928
Stevan Grujic <s.grujic@rs.emakina.com>
2021-05-15 23:16:21 +0200

    initial setup

---
+++ payments/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@grooyatickets/common';
+
+const app = express();
+//trust https proxy such as ngnix
+app.set('trust proxy', true);
+app.use(json());
+//disable encryption on cookie since JWT is already enrypted
+//cookie is only used over https connection
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
